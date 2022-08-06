ade28d9c1c60ddad72ff6f6c52d378ec27ca3b67
Priyaraj7 <suppichala@gmail.com>
2022-08-06 10:44:24 -0700

    add database and table file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,20 @@
-import express from "express";
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
-const app = express();
+const app: Express = express();
 const port = 5000;
+
+// interface Request<
+//   P = core.ParamsDictionary,
+//   ResBody = any,
+//   ReqBody = any,
+//   ReqQuery = core.Query
+// > extends core.Request<P, ResBody, ReqBody, ReqQuery> {}
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello");
 });
 
 app.listen(port, () => {
-  console.log(`server listening to the port ${port}`);
+  console.log(`⚡️[server]:server listening to the port ${port}`);
 });
