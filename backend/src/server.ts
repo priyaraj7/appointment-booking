4bb6995cb575422e6f4d5592c7e17a13bd906f32
Priyaraj7 <suppichala@gmail.com>
2022-08-07 11:18:21 -0700

    connecting database with express is working

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,42 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+
+dotenv.config();
+
+//import { db } from "../db/db-connection.js";
+//backend/db/db-connection.js
+
+import db from "./db/db-connection";
 
 const app: Express = express();
 const port = 5000;
 
-// interface Request<
-//   P = core.ParamsDictionary,
-//   ResBody = any,
-//   ReqBody = any,
-//   ReqQuery = core.Query
-// > extends core.Request<P, ResBody, ReqBody, ReqQuery> {}
+// ----------------------------
+// allow your Express server to use the cors middleware
+// app.use(cors);
+app.use(express.json());
+// ----------------------------
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello");
+  console.log("hehere");
+  return res.send("Hello");
+});
+
+// get request
+app.get("/api/users", cors(), async (req: Request, res: Response) => {
+  try {
+    const { rows: users } = await db.query("SELECT * FROM users");
+    console.log("user:", users);
+    res.send(users);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ err });
+  }
 });
 
 app.listen(port, () => {
+  //dotenv.config();
+  console.log(process.env.DATABASE_URL);
   console.log(`⚡️[server]:server listening to the port ${port}`);
 });
