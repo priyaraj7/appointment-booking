373cad500e5f9510b829ab517a4bd19baebf554f
Supriya <56571107+priyaraj7@users.noreply.github.com>
2022-08-24 14:24:50 -0700

    remove curly braces
    Co-authored-by: Chris Jaure <chrisjaure@gmail.com>

--- frontend/src/components/doctor/DetailDoctorPage.tsx
+++ frontend/src/components/doctor/DetailDoctorPage.tsx
@@ -96,7 +96,7 @@ export default function DetailDoctorPage() {
           }}
         >
           <Button
-            display={"flex"}
+            display="flex"
             justifyContent={"center"}
             alignItems={"center"}
             fontSize={"sm"}
